f9c21b7e1e6302b3872073529b303afac8f66576
freneticmango <79719031+freneticmango@users.noreply.github.com>
2024-07-19 14:57:06 -0700

    Write computer's choosing function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+function getComputerChoice() {
+    choose = Math.random()
+
+    if (choose < .3) {
+        computerChoice = 'rock'
+    } else if (choose > .3 && choose < .6) {
+        computerChoice = 'paper'
+    } else {
+        computerChoice = 'scissors'
+    }
+
+    return computerChoice
+}
+
+compChoice = getComputerChoice()
+console.log(compChoice)
